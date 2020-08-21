4b162ccfad7fb03203fc96c953c43ccc091e6230
pradeep joshi <loveniit01@gmail.com>
2020-08-21 09:42:13 +0530

    initial commit

---
+++ src/main/java/com/pj/learn/DockerSpringTomcatExampleApplication.java
@@ -0,0 +1,13 @@
+package com.pj.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringTomcatExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringTomcatExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pj/learn/DockerSpringTomcatExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringTomcatExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
